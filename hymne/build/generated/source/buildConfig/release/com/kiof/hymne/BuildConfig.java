1ed696c1f523e76e7876aa95988770683e226ec0
kiof <kiof@free.fr>
2014-12-05 20:30:46 +0100

    New Admob SDK

--- hymne/build/generated/source/buildConfig/release/com/kiof/hymne/BuildConfig.java
+++ hymne/build/generated/source/buildConfig/release/com/kiof/hymne/BuildConfig.java
@@ -5,9 +5,9 @@ package com.kiof.hymne;
 
 public final class BuildConfig {
   public static final boolean DEBUG = false;
-  public static final String PACKAGE_NAME = "com.kiof.hymne";
+  public static final String APPLICATION_ID = "com.kiof.hymne";
   public static final String BUILD_TYPE = "release";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 15;
-  public static final String VERSION_NAME = "1.95";
+  public static final int VERSION_CODE = 16;
+  public static final String VERSION_NAME = "1.96";
 }
